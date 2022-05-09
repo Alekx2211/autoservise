c41268bb267056323ec4382f412c62c1d245a8b4
Alekx2211 <42638496+Alekx2211@users.noreply.github.com>
2022-05-09 15:18:51 +0300

    апа

--- js/main.js
+++ js/main.js
@@ -1,27 +1,4 @@
-new Swiper ('.left',{
-    slidesPerView: 3,
-    spaceBetween: 30,
-    freeMode: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-})
-
-
-$('.right').slick({
-
-});
-
-$('.offers').slick({
-    infinite: true,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    variableWidth: true
-});
-$('.popular').slick({
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    variableWidth: true
+var mySwiper = new Swiper('.left-container', {
+    speed: 400,
+    spaceBetween: 100
 });
