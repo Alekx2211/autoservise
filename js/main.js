866e8446fb8c2a03c7618feda9deecd75713942c
Alekx2211 <42638496+Alekx2211@users.noreply.github.com>
2022-05-09 19:59:59 +0300

    qwe

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-new Swiper ('.left',{
-    slidesPerView: 3,
-    spaceBetween: 30,
-    freeMode: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-})
-
 
 $('.right').slick({
 
