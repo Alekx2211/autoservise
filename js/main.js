9a102efc17cdc2ebc47686ed441c9a2f2da5263d
Alekx2211 <42638496+Alekx2211@users.noreply.github.com>
2022-05-15 19:59:38 +0300

    12

--- js/main.js
+++ js/main.js
@@ -1,17 +1,37 @@
 
-$('.right').slick({
+var slider2 = new Swiper(".left", {
+    slidesPerView: 4,
+    spaceBetween: 15,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".next1",
+        prevEl: ".prev1",
+    },
+    loop: true,
+});
+var slider1 = new Swiper(".right", {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+        nextEl: ".next2",
+        prevEl: ".prev2",
+    },
+});
+var swiper3 = new Swiper(".offers", {
+    slidesPerView: 7.5,
+    spaceBetween: 15,
 
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true,
 });
 
-$('.offers').slick({
-    infinite: true,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    variableWidth: true
-});
-$('.popular').slick({
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    variableWidth: true
-});
